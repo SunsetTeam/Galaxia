7738957d45ee902806123ba4acb91c831a3a8687
= <uujuju41@gmail.com>
2023-02-11 10:46:02 -0300

    Items

--- src/galaxia/content/GalItems.java
+++ src/galaxia/content/GalItems.java
@@ -3,9 +3,14 @@ package galaxia.content;
 import mindustry.type.Item;
 
 public class GalItems {
-    public static Item none;
+    public static Item kalyenite, lonite, olynite, thirium, fors;
 
+    //TODO item costs
     public static void load() {
-
+        kalyenite = new Item("kalyenite");
+        lonite = new Item("lonite");
+        olynite = new Item("olynite");
+        thirium = new Item("thirium");
+        fors = new Item("fors");
     }
 }
