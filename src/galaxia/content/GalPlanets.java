eecb8caf95e1cd0076f528f7755ce3fae061bbe5
xStaBUx <buksta@outlook.com>
2023-02-11 14:31:03 +0200

    TODO

--- src/galaxia/content/GalPlanets.java
+++ src/galaxia/content/GalPlanets.java
@@ -1,11 +1,55 @@
 package galaxia.content;
 
-import mindustry.type.Planet;
+import arc.graphics.Color;
+import mindustry.content.*;
+import mindustry.graphics.Shaders;
+import mindustry.graphics.g3d.HexMesh;
+import mindustry.graphics.g3d.NoiseMesh;
+import mindustry.graphics.g3d.SunMesh;
+import mindustry.maps.generators.BlankPlanetGenerator;
+import mindustry.maps.planet.AsteroidGenerator;
+import mindustry.maps.planet.TantrosPlanetGenerator;
+import mindustry.type.*;
 
 public class GalPlanets {
-    public static Planet none;
+    public static Planet TODO, TODO1;
 
     public static void load() {
+        TODO = new Planet("null", Planets.sun, 4){{
+            bloom = true;
+            accessible = false;
+            drawOrbit = false;
+            orbitRadius = 5000;
 
+            solarSystem = this;
+
+            meshLoader = () -> new SunMesh(
+                    this, 4,
+                    5, 0.3, 1.7, 1.2, 1,
+                    1.1f,
+                  //TODO ned palette
+                    Color.valueOf("ffffff"),
+                    Color.valueOf("ffffff"),
+                    Color.valueOf("ffffff"),
+                    Color.valueOf("ffffff"),
+                    Color.valueOf("ffffff"),
+                    Color.valueOf("ffffff")
+            );
+        }};
+
+        TODO1 = new Planet("null-1", TODO, 1, 4){{
+           meshLoader = () -> new HexMesh(this, 4);
+           generator = new TantrosPlanetGenerator();//TODO
+
+           atmosphereColor = Color.valueOf("5ee5b1");
+           atmosphereRadIn = 0.02f;
+           atmosphereRadOut = 0.3f;
+
+           bloom = false;
+
+           alwaysUnlocked = true;
+
+           solarSystem = TODO;
+        }};
     }
 }
