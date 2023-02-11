619a47e9ce483918a6d9cb8ca043f9ad3f5bb7bc
B1ss1 <m1ss1onaryposethebestxxxx@gmail.com>
2023-02-11 17:39:58 +0300

    init items code

--- src/galaxia/content/GalItems.java
+++ src/galaxia/content/GalItems.java
@@ -3,11 +3,15 @@ package galaxia.content;
 import mindustry.type.Item;
 
 public class GalItems {
-    public static Item kalyenite, lonite, olynite, thirium, fors;
+    public static Item craside, kaylenite, hanshinium, lonite, olynite, thirium, fors;
 
     //TODO item costs
     public static void load() {
-        kalyenite = new Item("kalyenite");
+
+        craside = new Item("craside");
+        kaylenite = new Item("kalyenite");
+        hanshinium = new Item("hanshinium");
+
         lonite = new Item("lonite");
         olynite = new Item("olynite");
         thirium = new Item("thirium");
