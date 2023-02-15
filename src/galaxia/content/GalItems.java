cef74e64969b3b221e2b0be4a531c80c1cc00feb
B1ss1 <m1ss1onaryposethebestxxxx@gmail.com>
2023-02-15 10:16:59 +0300

    init items

--- src/galaxia/content/GalItems.java
+++ src/galaxia/content/GalItems.java
@@ -1,20 +1,51 @@
 package galaxia.content;
 
+import arc.graphics.Color;
 import mindustry.type.Item;
 
 public class GalItems {
-    public static Item craside, kaylenite, hanshinium, lonite, olynite, thirium, fors;
+    public static Item
+            //1
+            fors, thirium,
+    //2
+    craside, kaylenite, hanshinium, khalgon,
+    //3
+    lonite, olynite;
 
     //TODO item costs
     public static void load() {
 
-        craside = new Item("craside");
-        kaylenite = new Item("kaylenite");
-        hanshinium = new Item("hanshinium");
+        //1
+        fors = new Item("fors", Color.valueOf("f3b2c9")){{
+            hardness = 1;
+            cost = 1f;
+        }};
+        thirium = new Item("thirium", Color.valueOf("d6e6c6")){{
+            hardness = 1;
+            cost = 1;
+        }};
 
-        lonite = new Item("lonite");
-        olynite = new Item("olynite");
-        thirium = new Item("thirium");
-        fors = new Item("fors");
+        //2
+        craside = new Item("craside", Color.valueOf("aee4cc")){{
+            cost = 2f;
+        }};
+        kaylenite = new Item("kaylenite", Color.valueOf("522a3a"/*f6927e*/)){{
+            cost = 2f;
+        }};
+        hanshinium = new Item("hanshinium", Color.valueOf("c5d776")){{
+            cost = 1f; //or it'd better to make 2?
+        }};
+        khalgon = new Item("khalgon", Color.valueOf("e2a1f2")){{
+            cost = 3f;
+            hardness = 3;
+        }};
+
+        //3
+        lonite = new Item("lonite"){{
+            cost = 1f;
+        }};
+        olynite = new Item("olynite"){{
+            cost = 1f;
+        }};
     }
 }
