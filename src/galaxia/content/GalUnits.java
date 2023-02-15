0a3f0b277296ac19df21d6c50ed7080ad8f295d5
B1ss1 <m1ss1onaryposethebestxxxx@gmail.com>
2023-02-15 14:47:49 +0300

    init env,gyurzaSprites;

--- src/galaxia/content/GalUnits.java
+++ src/galaxia/content/GalUnits.java
@@ -4,6 +4,9 @@ import mindustry.type.UnitType;
 
 public class GalUnits {
     public static UnitType none;
+    //core units
+        //gryrza, veresk, vortex;
+    //
 
     public static void load() {
     }
