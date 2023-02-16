c43e2e7d35d9593078abda554a2bd5d0d60d8e76
B1ss1 <m1ss1onaryposethebestxxxx@gmail.com>
2023-02-16 15:00:15 +0300

    init(gyurzaTest);

--- src/galaxia/content/GalUnits.java
+++ src/galaxia/content/GalUnits.java
@@ -1,13 +1,30 @@
 package galaxia.content;
 
+import mindustry.ai.types.BuilderAI;
 import mindustry.type.UnitType;
 
 public class GalUnits {
     public static UnitType none;
-    //core units
-        //gryrza, veresk, vortex;
-    //
-
     public static void load() {
+        //core
+        UnitType gyurza = new UnitType("Gyurza") {{
+            aiController = BuilderAI::new;
+            isEnemy = false;
+
+            lowAltitude = true;
+            flying = true;
+            mineSpeed = 21.5f;
+            mineTier = 1;
+            buildSpeed = 0.35f;
+            drag = 0.05f;
+            speed = 4.3f;
+            rotateSpeed = 10f;
+            accel = 0.1f;
+            itemCapacity = 20;
+            health = 340f;
+            hitSize = 12f;
+            alwaysUnlocked = true;
+
+        }};
     }
 }
